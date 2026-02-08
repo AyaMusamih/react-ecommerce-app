79c78d450f399fdf31455d5688da999c13b9ebd2
Aya Musamih <msamehaya@gmail.com>
2026-02-08 23:14:50 +0200

    Fix Footer import case sensitivity

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from "react-router-dom";
-import Footer from "../components/footer/footer";
+import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
 
 export default function MainLayout() {
